8f0f5a93664af3c312f699f81c5e5a93b5e46521
Apoh <Edenra08@gmail.com>
2022-06-15 01:07:16 +0100

    Assignment Done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from "./Task"
 
-function TaskList() {
+
+
+function TaskList(TASKS ) {
+
+  const[deleteBtn, setDeleteBtn] = useState(false)
+
+
+function handleDeleteBtn(){
+setDeleteBtn((deleteBtn) => !deleteBtn)
+}
+
+  const tasksList= TASKS.map((task) => (
+      
+    <Task 
+    key={task.text} 
+    text={task.text} 
+    category={task.category}
+    deleteBtn ={deleteBtn}
+    handleDeleteBtn ={handleDeleteBtn}
+    />))
+
+
+
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+     {tasksList}
+     
     </div>
   );
 }
